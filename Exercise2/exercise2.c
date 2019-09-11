dd4e621f3445d5acc5b83106d60766c4644657b5
ARAdramala <55189126+ARAdramala@users.noreply.github.com>
2019-09-11 14:13:21 +0200

    Version 1

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,16 @@
 /* Here, you should entry the required libraries */
+#include <stdio.h>
 
 void main(){
 	/* Here, you should write the source code of your program */
+	int sumando, resultado;
+	
+	printf("Introduce the first number: ");
+	scanf("%d", &sumando);
+	resultado = sumando;
+	printf("Introduce the second number: ");
+	scanf("%d", &sumando);
+	resultado += sumando;
+	
+	printf("the result is %d", resultado);
 }
